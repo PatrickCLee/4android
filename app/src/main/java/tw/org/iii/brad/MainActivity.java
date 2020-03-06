29a3147729ec1ca1515dbe65dd90bd10a98cfe85
Ian <zeltkimo+github@gmail.com>
2020-03-06 11:33:59 +0800

    fin Mar01

--- app/src/main/java/tw/org/iii/brad/MainActivity.java
+++ app/src/main/java/tw/org/iii/brad/MainActivity.java
@@ -16,8 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //R是一個類別, R is a class, resource->layout->activity_main
         tv = findViewById(R.id.textView);
+        //找到在xml中設置的標籤,指派給變數tv,tv需事先宣告如上方private TextView
     }
 
     public void click(View view) {
@@ -25,7 +26,8 @@ public class MainActivity extends AppCompatActivity {
         while (set.size() < 6){
             set.add((int)(Math.random()*49+1));
         }
-        //Log.v("brad",set.toString());
+        Log.v("brad",set.toString());
+        //Log是常用除錯方式,配合Logcat視窗選擇範圍,右方下拉式選edit,命名並在Log tag輸入此處設的字串
         tv.setText(set.toString());
     }
 }
